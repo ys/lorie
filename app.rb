781e060201b076f1393669abe18a4514ccc446c3
Yannick Schutz <yannick.schutz@gmail.com>
2012-12-07 17:02:42 +0100

    MOAWR

--- app.rb
+++ app.rb
@@ -14,3 +14,10 @@ post '/' do
     slim :index
   end
 end
+
+WEEKENDS = ['http://www.youtube.com/embed/j0kiEwRI-rI?fs=1&feature=oembed&autoplay=1',
+            'http://www.youtube.com/embed/Meb7uaNlcS0?fs=1&feature=oembed&autoplay=1',
+            'http://www.youtube.com/embed/8PtGtcpGUyA?fs=1&feature=oembed&autoplay=1',
+            'http://www.youtube.com/embed/KyisIXCTU9U?fs=1&feature=oembed&autoplay=1',
+            'http://www.youtube.com/embed/SBamueZuEtQ?fs=1&feature=oembed&autoplay=1',
+            'http://www.youtube.com/embed/e2rjSKoq6ng?fs=1&feature=oembed&autoplay=1']
